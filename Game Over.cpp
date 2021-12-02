eb7af8f851ba5b2369092b943ee68e9dc5367d08
unknown <lucas.cassimiro@live.com>
2021-12-02 09:37:42 -0300

    Added several notes from the first pages of the book

--- Game Over.cpp
+++ Game Over.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+#include <iostream> //This is a header file and tells the preprocessor to include the contents from another file.
 
-int main ()
+int main () // All C++ programs must have a function called main(), this is the starting point of the program.
 {
-    std::cout << "Game Over!" << std::endl;
+    std::cout << "Game Over!" << std::endl; //shocking, this seems really long and different from C and Py.
     return 0;
 }
+
+//COMMENTS are made by using two forward slashes in a row .
+/*  THIS IS
+    A MULTIPLE LINE
+    COMMENT 
+*/
+
+/* All functions are delimited by a pair of curly braces. Code between two curly braces is called block
+and is usually indented to show that it forms a unit. The block of code that makes up an entire function
+is called the body of the function.*/
