c36ab43bf3d1ccaedc51d35dd190112e44bf525a
unknown <lucas.cassimiro@live.com>
2021-12-06 20:39:57 -0300

    Just some notes about what I'm learning

---
+++ Theory/Variables.cpp
@@ -0,0 +1,27 @@
+/* This is the basic of the theoretical part of the C++ programming.
+
+Variables are informations stored somewhere in memory.
+They don't mix, they can't mix
+
+They can be imagined as ink buckets
+    We can observe and we can change their values.
+
+The main variable types are:
+    > Boolean (0 or 1)
+    > Interger (ex: -5)
+    > Float (ex: 0,0042)
+    > String (Characteres, ex: N1CE&$WELL)
+    > Vector (3 floats, ex: 42, -4.2, 0.42)
+
+Hierarchy / Heritage
+
+Inherits properties
+It respects biology logic
+
+One example from biology:
+    Plants
+        Trees
+            Fructiferous
+                Apple Tree
+
+*/
