4dbe782e0f9e0da60b52bc69fc12ef3888fe4821
unknown <lucas.cassimiro@live.com>
2021-12-06 22:10:40 -0300

    adding notes

--- Game Over.cpp
+++ Game Over.cpp
@@ -14,4 +14,26 @@ int main () // All C++ programs must have a function called main(), this is the 
 
 /* All functions are delimited by a pair of curly braces. Code between two curly braces is called block
 and is usually indented to show that it forms a unit. The block of code that makes up an entire function
-is called the body of the function.*/
+is called the body of the function.
+
+"Game Over!" is a string, a series of printable characters. Technically, it's a string literal, meaning it's 
+literally the characters between the quotes.
+
+cout is an object, it was defined in the iostrem, it's used to send data to the standard output stream. The output
+stream simply means the console window on the computer screen.
+
+the output operator (<<) means that I'm sending data to cout. The output operator is like a funnel; it takes
+whatever's on the open side and funnels it to the pointy side.
+
+the prefix std in cout is to tell the compiler that I mean cout from the standard library. Std is a namespace.
+Namespaces are like area codes of a phone number. It identifies the group to wich something belongs.
+You prefix a namespace using the scope resolution operator (::).
+
+std::endl goes to the standard output. endl is defined in iostream and is also an object in the std namespace. 
+Sending endl to the standard output acts like pressing the Enter key in the console window.
+
+statements are the basic units controlling the execution flow and they all end with a semicolon. 
+
+return 0; this indicates that the program ended without a problem.
+
+*/
