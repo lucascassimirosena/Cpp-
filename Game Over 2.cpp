6c504ef0716b18e74004e270513665636522f63b
unknown <lucas.cassimiro@live.com>
2021-12-07 23:08:21 -0300

    Learning different ways to do the same thing

---
+++ Game Over 2.cpp
@@ -0,0 +1,16 @@
+//Game Over 2.0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Game Over!" << endl;
+    return 0;
+}
+
+/*
+
+when I typed "using namespace std;" it gave me direct access to elements of the std namespace.
+
+*/
