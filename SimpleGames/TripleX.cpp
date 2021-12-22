d9e6a69561016d22c8c8c0155509c5d004a963c6
unknown <lucas.cassimiro@live.com>
2021-12-22 18:42:29 -0300

    studied about veriables

--- SimpleGames/TripleX.cpp
+++ SimpleGames/TripleX.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+using namespace std;
+
 int main()
 {
+    cout << "You are about to enter a secret party" << endl;
+    cout << "The guard requests you the correct code to enter it..." << endl;
+
+    int a = 4;
+    int b = 2;
+    int c = 6;
+
+    int sum = a + b + c;
+    cout << sum << endl;
+    int product = a * b * c;
+    cout << product << endl;
     return 0;
 }
