2c16b3b7c689fa9d987ac4cc5cdfa40ab81046eb
unknown <lucas.cassimiro@live.com>
2021-12-21 23:33:59 -0300

    creating some tests and testing the compiler

---
+++ Theory/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "You are about to enter a secret party" << endl;
+    cout << "The guard requests you the correct code to enter it..." << endl;
+    return 0;
+}
