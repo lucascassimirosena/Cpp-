883cb5f1555feb28904ad6011f1eac6101bb2340
unknown <lucas.cassimiro@live.com>
2021-12-28 00:24:09 -0300

    corrected the const type

---
+++ Theory/TripleX.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "You are about to enter a secret party" << endl;
+    cout << "The guard requests you the correct code to enter it..." << endl;
+
+    const int a = 4;
+    const int b = 2;
+    const int c = 6;
+
+    //Variables can be changed but constants can't
+
+ 
+
+    int sum = a + b + c;
+    cout << sum << endl;
+
+    int product = a * b * c;
+    cout << product << endl;
+
+
+
+
+    return 0;
+}
