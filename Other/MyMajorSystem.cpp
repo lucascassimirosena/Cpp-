d3ed138ca73e6a76c716ec0af24e1f3440be04e4
unknown <lucas.cassimiro@live.com>
2021-12-28 22:20:05 -0300

    MyMajorSystem

---
+++ Other/MyMajorSystem.cpp
@@ -0,0 +1,55 @@
+/* Alright if you stumbled upon this file you must be asking yourself WHAT THE HELL DOES 
+"The Major System mean". I'll explain you in the following lines and then you can create your own */
+
+/* The Major System is nothing more than a memory technique that converts numbers into phonetic sounds.
+When we do it we can help our brains to remember them more easily.
+
+For instance, if you were to remember the number 20248321 you would find it more easily if you had
+imagined an story in your mind related to that number.
+
+For me these numbers have different meaning, for example:
+20 - Nasa
+24 - Nero
+83 - Game
+21 - Net
+
+Nasa invites Nero to play a Game called 'Net'.
+20248321.
+
+Why do I think about these meanings when I think about these numbers? Well as I said,
+the goal is to turn each number into a phonetic sound, we do it by converting numbers into letters:
+
+0 - "S", "SS"
+1 - "T" or "D"
+2 - "N"
+3 - "M"
+4 - "R"
+5 - "L"
+6 - "CH" , "SH" or "X"
+7 - "G" or "K"
+8 - "F" or "V"
+9 - "P" or "B"
+
+This is these are the basic building blocks of this technique. You can make your own rules.
+So here we go. I made the rest of the code just to help myself to remind my Major System. If you're
+going to start using this technique I suggest you to create your own. */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+char s = '0';
+char t = '1';
+char n = '2';
+char m = '3';
+char r = '4';
+char l = '5';
+char ch = '6';
+char g = '7';
+char f = '8';
+char p = '9';
+
+return 0;
+}
--- SimpleGames/TripleX.cpp
+++ SimpleGames/TripleX.cpp
@@ -6,13 +6,22 @@ int main()
     cout << "You are about to enter a secret party" << endl;
     cout << "The guard requests you the correct code to enter it..." << endl;
 
-    int a = 4;
-    int b = 2;
-    int c = 6;
+    const int a = 4;
+    const int b = 2;
+    const int c = 6;
+
+    //Variables can be changed but constants can't
+
+ 
 
     int sum = a + b + c;
     cout << sum << endl;
+
     int product = a * b * c;
     cout << product << endl;
+
+
+
+
     return 0;
 }
