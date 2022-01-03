5e98571f54aa462af3423887002dcc759fb6fb47
unknown <lucas.cassimiro@live.com>
2022-01-02 23:11:53 -0300

    Data Types HackerRank challenge

---
+++ HackerRank/dataTypes.cpp
@@ -0,0 +1,28 @@
+/* This challenge consisted on taking several types of data types and displaying them" */
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int num;
+    long numL;
+    char a;
+    float fl;
+    double db;
+    
+    cin >> num;
+    cin >> numL;
+    cin >> a;
+    cin >> fl;
+    cin >> db;
+    
+    cout << num << endl;
+    cout << numL << endl;
+    cout << a << endl;
+    cout << fl << endl;
+    cout << db << endl;
+    
+    
+    return 0;
+}
