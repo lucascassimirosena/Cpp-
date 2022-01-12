da9016abea1e23108f69533053cce778cfdd6597
unknown <lucas.cassimiro@live.com>
2022-01-12 20:01:21 -0300

    Well, I'm just testing a few things

--- Other/MyMajorSystem.cpp
+++ Other/MyMajorSystem.cpp
@@ -1,7 +1,7 @@
 /* Alright if you stumbled upon this file you must be asking yourself WHAT THE HELL DOES 
-"The Major System mean". I'll explain you in the following lines and then you can create your own */
+"The Major System mean". I'll explain you in the following lines and then you can create your own 
 
-/* The Major System is nothing more than a memory technique that converts numbers into phonetic sounds.
+ The Major System is nothing more than a memory technique that converts numbers into phonetic sounds.
 When we do it we can help our brains to remember them more easily.
 
 For instance, if you were to remember the number 20248321 you would find it more easily if you had
@@ -32,42 +32,162 @@ the goal is to turn each number into a phonetic sound, we do it by converting nu
 
 This is these are the basic building blocks of this technique. You can make your own rules.
 So here we go. I made the rest of the code just to help myself to remind my Major System. If you're
-going to start using this technique I suggest you to create your own. */
+going to start using this technique I suggest you to create your own. 
+
+
+After you build the first blocks of your major system, you need to combine the numbers
+and give a meaningfull mental picture to it, adding a name to each combination".
+The vowels will be used to complete the words.
+
+00 - Susi
+01 - Suit
+02 - Sun
+03 - Sam
+04 - Sour
+05 - Sole
+06 - Sasha
+07 - Sakê
+08 - Safe
+09 - Soup
+10 - Dose
+
+The less abstract your words are, the better, think about real objects or people you know
+and try to not put stuff like feelings in your major system because it makes it more difficult to 
+remember.
+
+11 - Dodo
+12 - Tin
+13 - Dome
+14 - Door
+15 - Tail
+16 - Dish
+17 - Dog
+18 - Dove
+19 - Tape
+20 - Nasa
+
+I'm going to write my entire system here, create your own, but now you got the idea.
+If we're trying to memorize a number like 20140016.
+
+We need to create a story, in this case it'd be:
+A Nasa's astronaut opened the Door for Susi and gave her a Dish.
+This way after you memorize your whole system, make it easier to remember big sequence of number and
+the crazier your story is, the better.
+20140016. I just typed it without checking for an answer.
+
+21 - Net
+22 - Nun
+23 - Nemo
+24 - Nero
+25 - Neil
+26 - Nicho
+27 - Nike
+28 - Navvy
+29 - Nape
+30 - Maze
+
+31 - MIT
+32 - Moon
+33 - Mime
+34 - Mario
+35 - Mail
+36 - Misha
+37 - Mug
+38 - Mafe
+39 - Map
+40 - Rose
+
+41 - Root
+42 - Ron
+43 - Rome
+44 - Rer
+45 - Rail
+46 - Rich
+47 - Rug
+48 - Rave
+49 - Rib
+50 - Lisa
+
+51 - Lit
+52 - Line
+53 - Lime
+54 - Lore
+55 - Lily
+56 - Lash
+57 - Leg
+58 - Leaf
+59 - Lip
+60 - Chess
+
+61 - Cheetah
+62 - Chin
+63 - Shame
+64 - Chair
+65 - Chile
+66 - Xuxa
+67 - Cheek
+68 - Chef
+69 - Ship
+70 - Gize
+
+71 - Gate
+72 - Gina
+73 - Game
+74 - Kira
+75 - Gail
+76 - Kash
+77 - Kiko
+78 - Cave
+79 - Gabi
+80 - Vase
+
+81 - VET
+82 - Vin 
+83 - Foam
+84 - VR
+85 - Vail
+86 - Fish
+87 - Vegie
+88 - Fifa
+89 - Vape
+90 - Piza
+
+91 - Bot
+92 - Bone
+93 - Pomme
+94 - Poor
+95 - Pale
+96 - Bush
+97 - Pug
+98 - Beef
+99 - Pope
+100 - Tesis
+*/
+
 
 #include <iostream>
 using namespace std;
 
-int main()
+int main ()
 {
 
-const int s = 0;
-const int t = 1;
-const int n = 2;
-const int m = 3;
-const int r = 4;
-const int l = 5;
-const int ch = 6;
-const int g = 7;
-const int f = 8;
-const int p = 9;
+    int Susi;
+ 
+    cout << "Well, let's start playing, Lucas, what number is Susi?" << endl;
+    cin >> Susi;
 
-int guess;
-
-cout << "Let's play a game just to warm up" << endl;
-cout << "I say a letter and you tell me the corresponding number in the major system." << endl;
-cout << "S: ";
-cin >> guess;
+    if (Susi == 0)
+    {
+        cout << "Very well, Lucas\n";
+    }
+    
+    else
+    {
+        cout << "No, that's not the answer. \n";
+    }
 
 
-if (guess == s)
-{
-   cout << "Well done!" << endl;
-}
-else
-{
-cout << "Try again." << endl;
+
+   return 0;
 }
 
-cout << 
-
-}
